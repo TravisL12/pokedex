cb84eb27b8d742dd0db56bba8f4f4f5f0c7b10b9
Travis Lawrence <travis.lawrence12@gmail.com>
2022-04-01 23:03:13 -0700

    show page

--- pages/pokemon/[pokemonName].tsx
+++ pages/pokemon/[pokemonName].tsx
@@ -2,17 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../styles/Home.module.css";
 
 const PokemonItem: NextPage = () => {
+  const [pokemonInfo, setPokemonInfo] = useState<any>();
   const router = useRouter();
   const { pokemonName } = router.query;
 
   const fetchPokemon = async () => {
     const resp = await fetch(`/api/pokemon/show?name=${pokemonName}`);
-    const data = await resp.json();
-    console.log(data);
+    const { pokemonResult } = await resp.json();
+    setPokemonInfo(pokemonResult);
   };
 
   useEffect(() => {
@@ -20,6 +21,12 @@ const PokemonItem: NextPage = () => {
       fetchPokemon();
     }
   }, [pokemonName]);
+
+  if (!pokemonInfo) {
+    return <h1>Loading...</h1>;
+  }
+
+  console.log(pokemonInfo, "pokemonInfo");
 
   return (
     <div className={styles.container}>
@@ -30,6 +37,24 @@ const PokemonItem: NextPage = () => {
       </Head>
       <Link href="/pokemon">Back to Pokemon List</Link>
       <h1>{pokemonName}: All Info</h1>
+      <div>
+        <strong>Stats</strong>
+        <ul>
+          {pokemonInfo?.stats.map((stat: any) => (
+            <li key={stat.stat.name}>
+              {stat.stat.name}: {stat.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <strong>Moves</strong>
+        <ul>
+          {pokemonInfo?.moves.map((move: any) => (
+            <li key={move.move.name}>{move.move.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
